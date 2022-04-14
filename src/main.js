ddaf518bf16c95e38b2defa76b0f8599a42377f1
kulkarnisheel@gmail.com <kulkarnisheel@gmail.com>
2022-04-13 19:57:48 -0700

    added first mod

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 // main.js
+// Total points = 5
+// mod 1: Allow the player to control the Rocket after it's fired (5)
+// mod 2: 
 let config = {
     type: Phaser.CANVAS,
     width: 640,
@@ -8,7 +11,7 @@ let config = {
 let game = new Phaser.Game(config);
 
 // reserve keyboard vars
-let keyF, keyR, keyLEFT, keyRIGHT;
+let keyF, keyR, keyLEFT, keyRIGHT, keyA, keyS, keyL;
 
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
